9062800c9faaa579ebba885ae70198718c962a73
trummelbummel <theresa.fruhwuerth@gmail.com>
2016-05-09 21:36:59 +0200

    clustering please change filepath at bottom if used

--- clustering/vis.py
+++ clustering/vis.py
@@ -1,16 +1,15 @@
 # Data handling
 import pandas as pd
 import numpy as np
-
+# filehandling
+import os
 # Outlier detection
 from sklearn import svm
-
 # Clustering
-# Kmeans
-from sklearn.cluster import KMeans
 #DBScan
 from sklearn.cluster import DBSCAN
 from sklearn import metrics
+
 
 #uebergabeparameter von website
 
@@ -23,8 +22,6 @@ num_links = 'AnzahlLinks'
 overlaps = 'overlaps'
 stretches = 'stretches'
 position = 'position'
-
-
 selectionlist = []
 selectionlist.extend((overlaps, stretches, position))
 
@@ -34,9 +31,13 @@ min_samplesparam = 3
 
 # read data,
 df1=pd.read_table('penalty_3.csv', sep=';',header=0)
+# all headers
+colnames = list(df1.columns.values)
 # slice data
-X = df1.ix[:,selectionlist]
-
+X=df1.ix[:,selectionlist]
+# rest indizes
+colnamesrest = [x for x in colnames if x not in selectionlist]
+Rest = df1.ix[:, colnamesrest]
 
 ##################################################3
 #
@@ -44,64 +45,56 @@ X = df1.ix[:,selectionlist]
 #
 ##################################################
 
+# dbscan using parameters given epsparameter is raidus, min sample is minimum points in a cluster
 db = DBSCAN(eps=epsparam, min_samples=min_samplesparam).fit(X)
+# extract clusterlabels and outliers
 core_samples = db.core_sample_indices_
+# define a boolean mask so we can plot the core, boundary, and noise points separately
+core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+core_samples_mask[db.core_sample_indices_] = True
 labels = db.labels_
 n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-print n_clusters_
-
-
 
 # returns clusters and outliers as lists
 clusters = [X[labels == i] for i in xrange(n_clusters_)]
-outliers = X[labels == -1]
-print("clusters start here")
-print(clusters)
-print('outliers start here')
-print(outliers)
+outliers = [X[labels == -1]]
 
-
-n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-# metrics / quality of clustering etc.
-print('Estimated number of clusters: %d' % n_clusters_)
-# print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
-# print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
-# print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
-# print("Adjusted Rand Index: %0.3f"
-#       % metrics.adjusted_rand_score(labels_true, labels))
-# print("Adjusted Mutual Information: %0.3f"
-#       % metrics.adjusted_mutual_info_score(labels_true, labels))
-print("Silhouette Coefficient: %0.3f"
-      % metrics.silhouette_score(X, labels))
-
-
-# Plot result
-import matplotlib.pyplot as plt
-
-# Black removed and is used for noise instead.
-unique_labels = set(labels)
-colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
-for k, col in zip(unique_labels, colors):
-    if k == -1:
-        # Black used for noise.
-        col = 'k'
-
-    class_member_mask = (labels == k)
-
-    xy = X[class_member_mask & core_samples_mask]
-    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col,
-             markeredgecolor='k', markersize=14)
-
-    xy = X[class_member_mask & ~core_samples_mask]
-    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col,
-             markeredgecolor='k', markersize=6)
-
-plt.title('Estimated number of clusters: %d' % n_clusters_)
-plt.show()
-
-
-###############################################
+# after clustering original indizes are still maintained
+# print("clusters start here")
+# print(clusters)
+# print('outliers start here')
+# print(outliers)
 #
-# SVM - can also be used to check for linear seperability when taking a linear svb kernel
-#
-###########################################
+# gives each cluster a running index
+def mergeDF(clusters, Rest):
+    dfoutlist = []
+    for i in range(len(clusters)):
+        dfout = pd.merge(clusters[i], Rest, how='outer', left_index = True, right_index =True)
+        dfoutnew = dfout.dropna()
+        dfoutnew.insert(0, 'innumcluster', [int(i)]*(len(dfoutnew)))
+        dfoutlist.append(dfoutnew)
+    return dfoutlist
+
+#outliers will be -1 index (i.e. -1 cluster)
+def mergeoutliersDF(outliers, Rest):
+    dfoutlist = []
+    for i in outliers:
+        dfout = pd.merge(i, Rest, how='outer', left_index = True, right_index =True)
+        dfoutnew = dfout.dropna()
+        dfoutnew.insert(0, 'innumcluster', [-1]*(len(dfoutnew)))
+        dfoutlist.append(dfoutnew)
+    return dfoutlist
+
+
+clusterdataframes = mergeDF(clusters, Rest)
+outlierdataframe = mergeoutliersDF(outliers, Rest)
+output = clusterdataframes + outlierdataframe
+clusterdataframes.append(outlierdataframe)
+framelist = [pd.DataFrame(x) for x in output]
+
+dftotaloutput = pd.concat(framelist)
+print(dftotaloutput)
+path = "/home/trummelbummel/Desktop/github/Forna_Clustering/clustering"
+
+# save to csv
+dftotaloutput.to_csv(path + "/results.csv")
