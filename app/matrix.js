11b4f163c0c23570efc0d94152497e17ef84ab6f
Viktoria Mayer <a1304613@unet.univie.ac.at>
2016-05-17 02:44:05 +0200

    Button zum Umschalten des Anzeigens von beste/schlechteste Visualisierungen, Umschaltung smoothig

--- app/matrix.js
+++ app/matrix.js
@@ -1,6 +1,7 @@
 function doMatrix() {
 
 	var svg = d3.select("#matrix");
+	var avgType=true;
 
 	var gewichtungen = [1,1,1];
 
@@ -20,68 +21,95 @@ function doMatrix() {
 	var stepSizes = [0.2,6.0,6.0];
 
 	for(var i=0;i<3;++i)
-	{
-
-		var iLowerRange=d3.min(results,function(d){return parseFloat(d[5+i]);});
-		var iUpperRange=parseFloat(d3.max(results,function(d){return d[5+i];}))+stepSizes[i];
-		var iRange=d3.scale.linear().domain([iLowerRange,iUpperRange]).range([0,100]);
-		var iStepSize=iRange(iLowerRange+stepSizes[i]);
-
-//		console.log(iLowerRange);
-//		console.log(iUpperRange);
-
 		for(var j=i+1;j<3;++j)
-		{
-			var visible = [];
-			visible.push(results[0]);
-	
-			for(var k=1;k<results.length;++k)
-			{
-				var v=false;
+			var group1 = svg.append("g")
+                		.attr("class","c"+i+"-"+j);	
 
-				for(var l=0;l<visible.length;++l)
+	var drawHeatmaps = function()
+	{
+		for(var i=0;i<3;++i)
+		{
+			var iLowerRange=d3.min(results,function(d){return parseFloat(d[5+i]);});
+			var iUpperRange=parseFloat(d3.max(results,function(d){return d[5+i];}))+stepSizes[i];
+			var iRange=d3.scale.linear().domain([iLowerRange,iUpperRange]).range([0,100]);
+			var iStepSize=iRange(iLowerRange+stepSizes[i]);
+
+//			console.log(iLowerRange);
+//			console.log(iUpperRange);
+
+			for(var j=i+1;j<3;++j)
+			{
+				var visible = [];
+				visible.push(results[0]);
+	
+				for(var k=1;k<results.length;++k)
 				{
-					if(visible[l][5+i]==results[k][5+i] && visible[l][5+j]==results[k][5+j])
+					var v=false;
+
+					for(var l=0;l<visible.length;++l)
 					{
-						v=true;
-						if(getAvgPenalty(results[k])<getAvgPenalty(visible[l]))
-							visible[l]=results[k];
-						break;
+						if(visible[l][5+i]==results[k][5+i] && visible[l][5+j]==results[k][5+j])
+						{
+							v=true;
+
+							if(avgType)
+								if(getAvgPenalty(results[k])<getAvgPenalty(visible[l]))
+									visible[l]=results[k];
+							else
+								if(getAvgPenalty(results[k])>getAvgPenalty(visible[l]))
+                	                                                visible[l]=results[k];
+							break;
+						}
 					}
+
+					if(!v)
+						visible.push(results[k]);
 				}
 
-				if(!v)
-					visible.push(results[k]);
+				var jLowerRange=d3.min(visible,function(d){return parseFloat(d[5+j]);});
+        	        	var jUpperRange=parseFloat(d3.max(visible,function(d){return d[5+j];}))+stepSizes[j];
+        		        var jRange=d3.scale.linear().domain([jLowerRange,jUpperRange]).range([0,100]);
+	                	var jStepSize=jRange(jLowerRange+stepSizes[j]);
+
+//				console.log(jLowerRange);
+//				console.log(jUpperRange);
+
+				var group1 = svg.selectAll(".c"+i+"-"+j);
+
+				var squares = group1.selectAll("rect").data(visible);
+				squares.enter().append("rect")
+					.attr("x",function(d){
+							return iRange(d[5+i]);
+						})
+					.attr("y",function(d){
+       		       	                		return jRange(d[5+j]);
+			                       	})
+					.attr("width",iStepSize)
+					.attr("height",jStepSize);
+
+				squares.transition().duration(1000)
+					.style("fill",function(d){
+        	               	        		return colorRange(getAvgPenalty(d));
+						});
+
+				group1.attr("transform","translate(" + i*130 + "," + (j-1)*130 + ")");
+
+				squares.exit().remove();
+
 			}
-
-			var group1 = svg.append("g");
-
-			var jLowerRange=d3.min(visible,function(d){return parseFloat(d[5+j]);});
-                	var jUpperRange=parseFloat(d3.max(visible,function(d){return d[5+j];}))+stepSizes[j];
-        	        var jRange=d3.scale.linear().domain([jLowerRange,jUpperRange]).range([0,100]);
-	                var jStepSize=jRange(jLowerRange+stepSizes[j]);
-
-//			console.log(jLowerRange);
-//			console.log(jUpperRange);
-
-			var squares = group1.selectAll("rect")
-				.data(visible)
-				.enter().append("rect")
-				.attr("x",function(d){
-						return iRange(d[5+i]);
-					})
-				.attr("y",function(d){
-       	       	                		return jRange(d[5+j]);
-		                       	})
-				.attr("width",iStepSize)
-				.attr("height",jStepSize)
-				.style("fill",function(d){
-                       	        		return colorRange(getAvgPenalty(d));
-					});
-
-			group1.attr("transform","translate(" + i*130 + "," + (j-1)*130 + ")");
 		}
-	}
+	};
+
+	drawHeatmaps();
+
+	d3.select("body").append("input")
+        .attr("value", "Change AvgPenalty")
+        .attr("type", "button")
+        .attr("class", "dataset-button")
+        .on("click", function(d) {
+                avgType=!avgType;
+		drawHeatmaps();
+        });
 	
 	/*svg.append("circle")
 		.attr("cx", 100)
