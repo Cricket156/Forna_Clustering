b6c983b155d075224e11b7e346d606c6b1d50405
Viktoria Mayer <a1304613@unet.univie.ac.at>
2016-05-27 13:22:01 +0200

    file handler.js zu letzten commit dazu

--- app/handler.js
+++ app/handler.js
@@ -77,6 +77,98 @@ function extractClusters() {
 			cluster.push(results[i]);
 		}
 	}
+	
+	var gewichtungen = [1,1,1];
+	
+	var getAvgPenalty = function(d) {
+		var result=0;
+		for(var i=0;i<gewichtungen.length;++i)
+			result+=gewichtungen[i]*d[2+i];
+		return result;
+	}
+	
+	clusters = clusters.sort(function(a, b) {
+			var a_avg = [];
+			a_avg.push(0);
+			a_avg.push(0);
+			var b_avg = [];
+			b_avg.push(0);
+			b_avg.push(0);
+			for(var i=0;i<3;++i)
+			{
+				a_avg.push(d3.mean(a,function(d) {
+						return parseFloat(d[2+i]);
+					}));
+				b_avg.push(d3.mean(b,function(d) {
+						return parseFloat(d[2+i]);
+					}));
+			}
+			
+			return getAvgPenalty(a_avg) - getAvgPenalty(b_avg);
+		});
+	
+	/*var counter = 0;
+	var len = 0;
+	// average cluster results
+	var cluster_penaltyarray = [];
+	var clusterlengtharray = [];
+	var totalposition = 0;
+	var totaloverlap = 0;
+	var totalstretches = 0;
+	var previous = 0;
+	var current = 0;
+	var globavg =0;
+	var globalsum = 0;
+	var avgpercluster = [];
+	var gobalavgarr = [];
+
+	// produce array with only penalties
+	clusters.forEach(function(d) {
+		// fore each node d in cluster
+		len = +d.length;
+		clusterlengtharray.push(len);
+		d.forEach(function(e){
+			// adds numcluster, overlaps, stretches, position
+			cluster_penaltyarray.push([+e[1], +e[2], +e[3], +e[4], len]);
+			 })
+			  });
+		// // gets average for each cluster penalties
+		// TODO: da ist irgendwo ein minifehler drinnen und bei den totaloverlaps wird irgendwo eins
+		// dazugezaehlt
+		// average penalties per cluster
+			// mean = d3.mean(selectedData,function(d) { return +d.reading})
+				cluster_penaltyarray.forEach(function(e){
+					current = e[0];
+					if (current == previous){
+						totaloverlap += e[1];
+						totalstretches += e[2];
+						totalposition += e[3];
+
+					}
+					else{
+						totaloverlap += e[1];
+						totalstretches += e[2];
+						totalposition += e[3];
+						len = +e[4];
+						avgpercluster.push([totaloverlap/len, totalstretches/len, totalposition/len]);
+						totaloverlap = 0;
+						totalstretches = 0;
+						totalposition = 0;
+
+					}
+					previous = current;
+
+			});*/
+	//console.log(avgpercluster)
+	// now average over all averages of clusters (global average)
+	//TODO: pushing global average into array doesn't work for reasons I can't understand
+	// avgpercluster.forEach(function(d){
+	//  globavg = ((+d[0] + +d[1] + +d[2])/cluster_count);
+	//  console.log(globavg);
+	//  // globalavgarr.push(globavg);
+	// });
+	//
+	//  console.log(globalavgarr);
 }
 
 function initDropdown() {
