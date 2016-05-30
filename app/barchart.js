ec78f42a375b27e88215ab554f8b4a8ea4a9fafc
trummelbummel <theresa.fruhwuerth@gmail.com>
2016-05-30 14:30:54 +0200

    achsen

--- app/barchart.js
+++ app/barchart.js
@@ -1,51 +1,56 @@
 function doBarchart() {
 	var svg = d3.select("#barchart");
-	
+
 	var clusters_filtered=[];
-	
+
 	var grenze=10;
 	if(clusters.length<10)
 		grenze=clusters.length;
-	
+
 	for(var i=0;i<grenze;++i)
 		clusters_filtered.push(clusters[i]);
-	
-	var m = [40, 40, 40, 40];
-	
+
+	var m = [40, 50, 40, 40];
+
 	var width = document.getElementById('barchartDiv').clientWidth - m[1] - m[3];
 	var height = (window.innerHeight-60)*(1/3) - m[0] - m[2];
-	
+
 	var axis_distance=30;
 
 	var cluster_count=clusters_filtered.length;
-	
+
 	var x_scale = d3.scale.linear()
-                .domain([0, cluster_count])
-                .range([0, width]);
+								.domain([0, cluster_count])
+								.range([0, width]);
 
 	var y_scale = d3.scale.linear()
 		.domain([0, d3.max( clusters_filtered, function (d) { return d.length;})])
-		.range([0, height]);
-		
+		.range([height, 0]);
+
 	var x_axis = d3.svg.axis()
 		.scale(x_scale)
 		.orient("bottom");
 
-//ticks y axis
-	var y_axisleft = d3.svg.axis().orient("left");
+
+	// var y_axisleft = d3.svg.axis().orient("left");
+
+	var y_axisleft = d3.svg.axis()
+		.scale(y_scale)
+		.orient("left")
+		.ticks(10);
 
 	var y_axisright = d3.svg.axis()
 		.scale(y_scale)
 		.orient("right")
 		.ticks(10);
 
-		
+
 	var svg = d3.select("#barchart")
 		.attr("width", width + m[3] + m[1])
 	    .attr("height", height + m[0] + m[2]);
-	
+
 	var wrapper = svg.append("g");
-	
+
 	var draw = wrapper.append("g")
 		.attr("transform", "translate(" + m[3] + "," + m[0] + ")");
 
@@ -59,30 +64,23 @@ function doBarchart() {
 		.attr("class", "xaxis")
 		.attr("transform", "translate(0," + height + ")")
 		.call(x_axis);
+		// .select("text")
+		// .attr("y", width+36)
+		// .attr("x", height/2)
+		// .attr("font-size","15px")
+		// .attr("transform", "rotate(-90)")
+		// .text("Overlaps");
 
+		draw.append("g")
+			.attr("class", "yaxisright")
+			.attr("transform", "translate(" + width + ",0)")
+			.call(y_axisright);
 
-	draw.append("g")
-		.attr("class", "yaxisright")
-		// .attr("transform", "translate(0," + height/2 + ")")
-		.call(y_axisright)
-		.select("text")
-			.attr("y", width-20)
-			.attr("dy", 6)
-			.attr("font-size","15px")
-			.attr("transform", "rotate(-90)");
-/*
-	svg.append("g")
-		.attr("class", "yaxisleft")
-		.attr("transform", "translate(0," + height/2 + ")")
-		.call(y_axisleft)
-		.select("text")
-			.attr("y", width+20)
-			.attr("dy", 6)
-			.attr("font-size","15px")
-			.attr("transform", "rotate(-90)")
-			.text("Frequency");
-
-*/
+			draw.append("g")
+				.attr("class", "yaxisleft")
+				// zweites argument darf nur zahl sein
+				.attr("transform", "translate(" + 0 + ",0)")
+				.call(y_axisleft);
 
 	var overlaps_scale = d3.scale.linear()
                 .domain([0, d3.max( clusters_filtered, function(d) {
@@ -168,11 +166,11 @@ function doBarchart() {
                 .attr("height", function(d) {
                                 return y_scale(d.length);
                         });
-		
+
 	svg.selectAll("rect")
 		.on("click", function(d) {
 				//d3.select("#matrix").selectAll("rect").style("fill", );
-				
+
 				cluster=d[0][1];
 
 				if(cluster==matrixfilter)
@@ -182,7 +180,7 @@ function doBarchart() {
 					doMatrix();
 					doParallelCoordinates();
 					rangeschanged=false;
-					
+
 					svg.selectAll("rect").each(function(d) {
 						if(cluster != d[0][1]) {
 								d3.select(this).style("opacity", 1.0);
@@ -198,14 +196,14 @@ function doBarchart() {
 					rangeschanged=false;
 
 					d3.select("#matrix").selectAll("rect").style("opacity",1.0);
-					
+
 					svg.selectAll("rect").each(function(d) {
 						if(cluster != d[0][1]) {
 								d3.select(this).style("opacity", 0.5);
 						}
 					});
 				}
-							
+
 				d3.select("#matrix").selectAll("rect").style("opacity",1.0);
 
 				d3.event.stopPropagation();
@@ -224,7 +222,7 @@ function doBarchart() {
 							}
 					});
 				}
-					
+
 				d3.event.stopPropagation();
 			})
 		.on("mouseout", function(d) {
@@ -237,7 +235,7 @@ function doBarchart() {
 							}
 						});
 				}
-					
+
 				d3.event.stopPropagation();
 			});
 
