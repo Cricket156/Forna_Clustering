334118796c44d9a0516f766350ed6f8ef438f6f6
Viktoria Mayer <a1304613@unet.univie.ac.at>
2016-06-04 17:41:06 +0200

    Scatterplot (teilweise von Theresa

--- app/matrix.js
+++ app/matrix.js
@@ -2,41 +2,41 @@ function doMatrix() {
 
 	var svg = d3.select("#matrix");
 	var svg_direct = svg;
-	
+
 	svg.select(".new").remove();
-	
+
 	if(!matrixloaded)
 	{
 		svg = svg.append("g");
 		for(var i=5;i<columnnames.length-1;++i)
 			for(var j=i+1;j<columnnames.length-1;++j)
 				var group1 = svg.append("g")
-                			.attr("class","c"+i+"-"+j);	
+                			.attr("class","c"+i+"-"+j);
 		matrixloaded=true;
 	}
 	else
 		svg = svg.select("g");
-		
+
 	svg.selectAll("g").selectAll(".axis").remove();
-	
+
 	var marginSide = 10,
 		marginBottom = 10,
 		marginTop = 10;
-	
+
 	var width = document.getElementById('matrixDiv').clientWidth;
 	var height = (window.innerHeight-60)*(2/3);
-	
+
 	svg_direct.attr("width", width)
 		.attr("height", width);
-		
+
 	width = width - marginSide - marginSide;
 	height = height - marginBottom - marginTop;
-		
+
 	svg.attr("transform", "translate(" + marginSide + "," + marginTop + "),scale("+width/((20 + 130*(columnnames.length-1-5-1)))+","+width/((20 + 130*(columnnames.length-1-5-1)))+")");
-	
+
 	//Iwo muss gespeichert sein, wie die StepSize beim Generieren war (oder iwie ausrechnen)
 	var stepSizes = [0,0,1,1,1];
-	
+
 	for(var i=0;i<columnnames.length-1;++i)
 	{
 		min1 = d3.min(results,function(d) {
@@ -48,7 +48,7 @@ function doMatrix() {
 			});
 		stepSizes.push(min2-min1);
 	}
-	
+
 	var avgType=true;
 
 	var gewichtungen = [1,1,1];
@@ -59,19 +59,19 @@ function doMatrix() {
 			result+=gewichtungen[i]*d[2+i];
 		return result;
 	}
-	
+
 	var drawRectangles = function(data,i,j,new_group) {
-		
+
 		var iLowerRange=d3.min(data,function(d){return parseFloat(d[i]);});
 		var iUpperRange=parseFloat(d3.max(data,function(d){return d[i];}))+stepSizes[i];
 		var iRange=d3.scale.linear().domain([iLowerRange,iUpperRange]).range([0,100]);
 		var iStepSize=iRange(iLowerRange+stepSizes[i]);
-			
+
 		var jLowerRange=d3.min(data,function(d){return parseFloat(d[j]);});
 		var jUpperRange=parseFloat(d3.max(data,function(d){return d[j];}))+stepSizes[j];
 		var jRange=d3.scale.linear().domain([jLowerRange,jUpperRange]).range([0,100]);
 		var jStepSize=jRange(jLowerRange+stepSizes[j]);
-	
+
 		var xaxis = d3.svg.axis()
 			.scale(iRange)
 			.orient("top")
@@ -81,7 +81,7 @@ function doMatrix() {
 			.scale(jRange)
 			.orient("left")
 			.ticks(5);
-	
+
 		var visible = [];
 		visible.push(data[0]);
 
@@ -126,7 +126,7 @@ function doMatrix() {
 
 		if(j>=5)
 		{
-		
+
 			var squares = group1.selectAll("rect").data(visible);
 			squares.style("fill",function(d){
 					return colorRange(getAvgPenalty(d));
@@ -146,11 +146,11 @@ function doMatrix() {
 								})
 				.attr("width",iStepSize)
 				.attr("height",jStepSize);
-			
+
 			group1.append("g")
 				.attr("class", "xaxis axis")
 				.call(xaxis);
-			
+
 			group1.append("g")
 				.attr("class", "yaxisleft axis")
 				.call(yaxisleft);
@@ -168,7 +168,7 @@ function doMatrix() {
 				});
 
 			squares.exit().remove();
-									
+
 		}
 		else
 		{
@@ -196,15 +196,16 @@ function doMatrix() {
 							else
 								return iStepSize/4;
 					});
-				
-			circles.exit().remove();
+
+
+			 circles.exit().remove();
 		}
 
 		if(!new_group)
 			group1.attr("transform","translate(" + (20 + (i-5)*130) + "," + (20 + (j-1-5)*130) + ")");
-		
+
 	}
-	
+
 	var colorRange=d3.scale.linear().
 			domain([d3.min(results,function(d){return getAvgPenalty(d);}),
 				d3.max(results,function(d){return getAvgPenalty(d);})])
@@ -213,7 +214,7 @@ function doMatrix() {
 	var drawHeatmaps = function(data)
 	{
 		var new_heatmap=true;
-		
+
 		for(var i=5;i<columnnames.length-1;++i)
 		{
 			if((-1==heatmapfilteri || -1==heatmapfilterj) || i==heatmapfilteri)
@@ -227,35 +228,111 @@ function doMatrix() {
 					if((-1==heatmapfilteri || -1==heatmapfilterj) || j==heatmapfilterj)
 					{
 						new_heatmap=false;
-						
+
 						drawRectangles(data,i,j,false)
 					}
 				}
 			}
 		}
-	
+
 		if(-1!=heatmapfilteri && -1!=heatmapfilterj)
 		{
-			if(new_heatmap)
+
+			if(heatmapfilterj<5)
 			{
 				svg.selectAll("*").selectAll("rect").remove();
 				svg.selectAll("*").selectAll(".xaxis").remove();
 				svg.selectAll("*").selectAll(".yaxisleft").remove();
+
+				svg.append("g").attr("class","new");
+				group1 = svg.select(".new");
+
+				var i=heatmapfilteri;
+				var j=heatmapfilterj;
+
+				var iLowerRange=d3.min(data,function(d){return parseFloat(d[i]);});
+				var iUpperRange=parseFloat(d3.max(data,function(d){return d[i];}))+stepSizes[i];
+				var iRange=d3.scale.linear().domain([iLowerRange,iUpperRange]).range([0,100]);
+				var iStepSize=iRange(iLowerRange+stepSizes[i]);
+
+				var jLowerRange=d3.min(data,function(d){return parseFloat(d[j]);});
+				var jUpperRange=parseFloat(d3.max(data,function(d){return d[j];}))+stepSizes[j];
+				var jRange=d3.scale.linear().domain([jLowerRange,jUpperRange]).range([0,100]);
+				var jStepSize=jRange(jLowerRange+stepSizes[j]);
+
+				var xaxis = d3.svg.axis()
+					.scale(iRange)
+					.orient("top")
+					.ticks(5);
+
+				var yaxisleft = d3.svg.axis()
+					.scale(jRange)
+					.orient("left")
+					.ticks(5);
+
+				group1.append("g")
+					.attr("class", "xaxis axis")
+					.call(xaxis);
+
+				group1.append("g")
+					.attr("class", "yaxisleft axis")
+					.call(yaxisleft);
+					
+				var colorRangeScatter=d3.scale.linear().
+					domain([-1,clusters.length])
+					.range([["blue","yellow"]]);
+					
+				//Scatterplot
+				//alles, was mit dem scatterplot zu tun hat, kommt in group1
+				//Die daten für die x-Achse stehen an der stelle i, die für die y-Achse stehen an der Stelle j
+				// Achsen scatterplot
+				// circles in scatterplot
 				
-				drawRectangles(data,heatmapfilteri,heatmapfilterj,true);
-				var group = svg.select(".new");
-				group.attr("transform","scale(" + (columnnames.length-1-5-1) + "," + (columnnames.length-1-5-1) + ")");
+				console.log(data);
 				
+				group1.selectAll(".dot")
+					.data(data)
+					.enter().append("circle")
+					.attr("class", "dot")
+					.attr("r", 3.5)
+					.attr("cx", function(d) { return iRange(d[i]); })
+					.attr("cy", function(d) { return jRange(d[j]); })
+					.style("opacity",0.2)
+					.style("fill", function(d) { 
+									/*if(-1==d[1])
+										return "black";
+									return "blue";*/
+									
+									return colorRangeScatter(d[1]);
+								});
+
+				group1.attr("transform","scale(" + ((20 + 130*(columnnames.length-1-5-1))/130) + "," + ((20 + 130*(columnnames.length-1-5-1))/130) + "),translate(20,20)");
 			}
 			else
 			{
-				svg.selectAll("*:not(.c"+heatmapfilteri+"-"+heatmapfilterj+")").selectAll("rect").remove();
-				svg.selectAll("*:not(.c"+heatmapfilteri+"-"+heatmapfilterj+")").selectAll(".xaxis").remove();
-				svg.selectAll("*:not(.c"+heatmapfilteri+"-"+heatmapfilterj+")").selectAll(".yaxisleft").remove();
+				if(new_heatmap)
+				{
+					svg.selectAll("*").selectAll("rect").remove();
+					svg.selectAll("*").selectAll(".xaxis").remove();
+					svg.selectAll("*").selectAll(".yaxisleft").remove();
 
-				var group = svg.selectAll(".c"+heatmapfilteri+"-"+heatmapfilterj);
-				group.attr("transform","scale(" + ((20 + 130*(columnnames.length-1-5-1))/130) + "," + ((20 + 130*(columnnames.length-1-5-1))/130) + "),translate(20,20)");
-				//group.transition().duration(1000).attr("transform","scale(2,2)");
+					drawRectangles(data,heatmapfilteri,heatmapfilterj,true);
+					var group = svg.select(".new");
+					group.attr("transform","scale(" + ((20 + 130*(columnnames.length-1-5-1))/130) + "," + ((20 + 130*(columnnames.length-1-5-1))/130) + "),translate(20,20)");
+
+				}
+				else
+				{
+					svg.selectAll("*:not(.c"+heatmapfilteri+"-"+heatmapfilterj+")").selectAll("rect").remove();
+					svg.selectAll("*:not(.c"+heatmapfilteri+"-"+heatmapfilterj+")").selectAll(".xaxis").remove();
+					svg.selectAll("*:not(.c"+heatmapfilteri+"-"+heatmapfilterj+")").selectAll(".yaxisleft").remove();
+
+					var group = svg.selectAll(".c"+heatmapfilteri+"-"+heatmapfilterj);
+					group.attr("transform","scale(" + ((20 + 130*(columnnames.length-1-5-1))/130) + "," + ((20 + 130*(columnnames.length-1-5-1))/130) + "),translate(20,20)");
+					//group.transition().duration(1000).attr("transform","scale(2,2)");
+				}
+
+
 			}
 		}
 
