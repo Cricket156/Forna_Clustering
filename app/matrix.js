5ea41156f9421e7ff0b2061c3ca6dee3e2aeda6c
Viktoria Mayer <a1304613@unet.univie.ac.at>
2016-06-19 11:35:49 +0200

    Scatterplot Code nicht extra

--- app/matrix.js
+++ app/matrix.js
@@ -56,7 +56,7 @@ function doMatrix() {
 					return d[(anzahlPenalties+2)+i];
 			});
 		
-		if(ifNaN(min2))
+		if(isNaN(min2))
 			stepSizes.push(0);
 		else
 			stepSizes.push(min2-min1);
@@ -71,7 +71,7 @@ function doMatrix() {
 		return result;
 	}
 
-	var drawRectangles = function(data,i,j,new_group) {
+	var drawDatapoints = function(data,i,j,new_group, isScatterplot) {
 
 		var iLowerRange=d3.min(original_results,function(d){return parseFloat(d[i]);});
 		var iUpperRange=parseFloat(d3.max(original_results,function(d){return d[i];}))+stepSizes[i];
@@ -132,25 +132,62 @@ function doMatrix() {
 			group1 = svg.select(".c"+i+"-"+j);
 		}
 
-		var squares = group1.selectAll("rect").data(visible);
-		squares.style("fill",function(d){
-				return colorRange(getAvgPenalty(d));
-			});
-		squares.enter().append("rect")
-			.style("fill",function(d){
+		var squares;
+		
+		if(isScatterplot)
+		{
+			//jLowerRange=0;
+			//jUpperRange=parseFloat(d3.max(original_results,function(d){return d[j];}))+stepSizes[j];
+			//jRange=d3.scale.linear().domain([jLowerRange,jUpperRange]).range([0,100]);
+			//jStepSize=jRange(jLowerRange+stepSizes[j]);
+		
+			group1.selectAll(".dot")
+				.data(data)
+				.enter().append("circle")
+				.attr("class", "dot")
+				.attr("r", 3.5)
+				.attr("cx", function(d) { return iRange(d[i]); })
+				.attr("cy", function(d) { return jRange(d[j]); })
+				.style("opacity",0.2)
+				.style("fill", function(d, i) {
+						if(-1==d[1])
+						{
+							if (document.getElementById("inputCheckbox").checked) {
+								d3.select(this).style("opacity", 0.0);
+							}
+							else
+							{
+								return "black";
+							}
+						}
+						else
+						{
+							return clusterColors[(results[i][1])];
+						}
+					});
+		}
+		else
+		{
+			squares = group1.selectAll("rect").data(visible);
+			squares.style("fill",function(d){
 					return colorRange(getAvgPenalty(d));
 				});
-		squares.attr("class",function(d){
-					return "r"+d[0]+" i"+i+" j"+j;
-				})
-			.attr("x",function(d){
-					return iRange(d[i]);
-				})
-			.attr("y",function(d){
-									return jRange(d[j]);
-							})
-			.attr("width",iStepSize)
-			.attr("height",jStepSize);
+			squares.enter().append("rect")
+				.style("fill",function(d){
+						return colorRange(getAvgPenalty(d));
+					});
+			squares.attr("class",function(d){
+						return "r"+d[0]+" i"+i+" j"+j;
+					})
+				.attr("x",function(d){
+						return iRange(d[i]);
+					})
+				.attr("y",function(d){
+										return jRange(d[j]);
+								})
+				.attr("width",iStepSize)
+				.attr("height",jStepSize);
+		}
 
 		group1.append("g")
 			.attr("class", "xaxis axis")
@@ -224,17 +261,13 @@ function doMatrix() {
 		{
 			if((-1==heatmapfilteri || -1==heatmapfilterj) || i==heatmapfilteri)
 			{
-
-//				console.log(iLowerRange);
-//				console.log(iUpperRange);
-
 				for(var j=i+1;j<=columnnames.length-1;++j)
 				{
 					if((-1==heatmapfilteri || -1==heatmapfilterj) || j==heatmapfilterj)
 					{
 						new_heatmap=false;
 
-						drawRectangles(data,i,j,false)
+						drawDatapoints(data,i,j,new_heatmap,false)
 					}
 				}
 			}
@@ -250,85 +283,7 @@ function doMatrix() {
 				svg.selectAll("*").selectAll(".xaxis").remove();
 				svg.selectAll("*").selectAll(".yaxisleft").remove();
 			
-				svg.append("g").attr("class","new");
-				group1 = svg.select(".new");
-				
-				var i=heatmapfilteri;
-				var j=heatmapfilterj;
-				
-				var iLowerRange=d3.min(data,function(d){return parseFloat(d[i]);});
-				var iUpperRange=parseFloat(d3.max(data,function(d){return d[i];}))+stepSizes[i];
-				var iRange=d3.scale.linear().domain([iLowerRange,iUpperRange]).range([0,100]);
-				var iStepSize=iRange(iLowerRange+stepSizes[i]);
-
-				var jLowerRange=d3.min(data,function(d){return parseFloat(d[j]);});
-				var jUpperRange=parseFloat(d3.max(data,function(d){return d[j];}))+stepSizes[j];
-				var jRange=d3.scale.linear().domain([jLowerRange,jUpperRange]).range([0,100]);
-				var jStepSize=jRange(jLowerRange+stepSizes[j]);
-
-				var xaxis = d3.svg.axis()
-					.scale(iRange)
-					.orient("top")
-					.ticks(5);
-
-				var yaxisleft = d3.svg.axis()
-					.scale(jRange)
-					.orient("left")
-					.ticks(5);
-					
-				group1.append("g")
-					.attr("class", "xaxis axis")
-					.call(xaxis)
-					.append("text")
-				//	.attr("class", "label")
-					.attr("y", -20)
-				//	.attr("x", -70)
-					.style("text-anchor", "middle")
-					.text(String(columnnames[i]));
-
-				group1.append("g")
-					.attr("class", "yaxisleft axis")
-					.call(yaxisleft)
-					.append("text")
-				//	.attr("class", "label")
-					.attr("transform", "rotate(-90)")
-					.attr("y", -30)
-					.attr("x", -70)
-					.attr("dy", ".71em")
-					.style("text-anchor", "middle")
-					.text(String(columnnames[j]));
-					
-				//Scatterplot
-				//alles, was mit dem scatterplot zu tun hat, kommt in group1
-				//Die daten für die x-Achse stehen an der stelle i, die für die y-Achse stehen an der Stelle j
-				// circles in scatterplot
-
-				group1.selectAll(".dot")
-					.data(data)
-					.enter().append("circle")
-					.attr("class", "dot")
-					.attr("r", 3.5)
-					.attr("cx", function(d) { return iRange(d[i]); })
-					.attr("cy", function(d) { return jRange(d[j]); })
-					.style("opacity",0.2)
-					.style("fill", function(d, i) {
-							if(-1==d[1])
-							{
-								if (document.getElementById("inputCheckbox").checked) {
-									d3.select(this).style("opacity", 0.0);
-								}
-								else
-								{
-									return "black";
-								}
-							}
-							else
-							{
-								return clusterColors[(results[i][1])];
-							}
-						});
-
-				group1.attr("transform","scale(" + ((marginSide + (100+marginSide)*(columnnames.length-1-(anzahlPenalties+2)-1))/(100+marginSide)) + "," + ((marginTop + (100+marginSide)*(columnnames.length-1-(anzahlPenalties+2)-1))/(100+marginSide)) + "),translate(" + marginSide + "," + marginTop + ")");
+				drawDatapoints(data,i,j,true,true);
 			}
 			else
 			{
@@ -340,7 +295,7 @@ function doMatrix() {
 					svg.selectAll("*").selectAll(".xaxis").remove();
 					svg.selectAll("*").selectAll(".yaxisleft").remove();
 					
-					drawRectangles(data,heatmapfilteri,heatmapfilterj,true);
+					drawDatapoints(data,heatmapfilteri,heatmapfilterj,true,false);
 					var group = svg.select(".new");
 					group.attr("transform","scale(" + ((marginSide + (100+marginSide)*(columnnames.length-1-(anzahlPenalties+2)-1))/(100+marginSide)) + "," + ((marginTop + (100+marginSide)*(columnnames.length-1-(anzahlPenalties+2)-1))/(100+marginSide)) + "),translate(" + marginSide + "," + marginTop + ")");
 				}
