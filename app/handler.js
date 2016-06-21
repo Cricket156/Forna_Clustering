f4951a6919ea60ace5ed367c62a8c488e9c27bab
Viktoria Mayer <a1304613@unet.univie.ac.at>
2016-06-21 23:28:05 +0200

    Richtiges File fuer Detailview

--- app/handler.js
+++ app/handler.js
@@ -257,17 +257,15 @@ function showSVG(d, svg_direct) {
 			
 			//window.open("file:///" + svgPfad + "/svg" + d[0] + ".svg");
 			
-			d3.xml("svg" + d[0] + ".svg", "image/svg+xml", function(error, xml) {
+			d3.xml("file:///" + svgPfad + "/svg" + (parseFloat(d[0])+1) + ".svg", "image/svg+xml", function(error, xml) {
 					if (error) throw error;
 
 					var svgNode = xml.getElementsByTagName("svg")[0];
 					svg.node().appendChild(svgNode);
 					svg.select("#plotting-area").select("g")
 						.attr("transform","scale(0.50,0.50)")
-						.on("click",function(d) {
-							//window.open("./svg1.svg");
-							console.log("new window");
-							window.open("file:///" + svgPfad + "/svg" + d[0] + ".svg");
+						.on("click",function() {
+							window.open("file:///" + svgPfad + "/svg" + (parseFloat(d[0])+1) + ".svg");
 						});
 					});
 		}
